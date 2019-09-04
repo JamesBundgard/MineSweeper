2b9dfd2fc0c78b5dca5736ae95a502a52c52cc60
JamesBundgard <jamesbundgard@gmail.com>
2019-09-03 21:58:13 -0400

    Progress on the layout. Grid can now be generated given the number of columns and rows.

--- app/src/main/java/com/example/minesweeper/MainActivity.kt
+++ app/src/main/java/com/example/minesweeper/MainActivity.kt
@@ -1,39 +1,67 @@
 package com.example.minesweeper
 
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.GridView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val numColumns = 10
+        val numRows = 10
+        val itemList = Array(numColumns * numRows) {i -> i.toString()}
+
+        val adapter = ImageListAdapter(this, R.layout.tile, itemList)
+        val gameGrid: GridView = findViewById(R.id.grid)
+
+        gameGrid.numColumns = numColumns
+        gameGrid.columnWidth = GridView.AUTO_FIT
+        gameGrid.stretchMode = GridView.STRETCH_COLUMN_WIDTH
+        gameGrid.adapter = adapter
+
+        gameGrid.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
+            println(parent.toString() + position.toString() + v.toString() + id.toString())
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
+
+internal class ImageListAdapter internal constructor(context: Context, private val resource: Int, private val itemList: Array<String>?) : ArrayAdapter<ImageListAdapter.ItemHolder>(context, resource) {
+
+    override fun getCount(): Int = if (this.itemList != null) this.itemList.size else 0
+
+    override fun getView(position: Int, view: View?, parent: ViewGroup): View {
+        var convertView = view
+        val holder: ItemHolder
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(resource, null)
+            holder = ItemHolder()
+            holder.name = convertView!!.findViewById(R.id.tile)
+            convertView.tag = holder
+        } else {
+            holder = convertView.tag as ItemHolder
+        }
+
+        holder.name!!.text = this.itemList!![position]
+
+        return convertView
+    }
+
+    internal class ItemHolder {
+        var name: TextView? = null
+    }
+}
+
+
